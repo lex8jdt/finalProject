2a6ddcd765279fc60b492e6f40aadeab4d15a754
DAXlTO <daxyt98@gmail.com>
2024-11-23 12:40:22 +0100

    [JIRA-1] Faltaba un import!!!

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from services.weatherService import WeatherService
 from services.coordinatesService import CoordinatesService
 
